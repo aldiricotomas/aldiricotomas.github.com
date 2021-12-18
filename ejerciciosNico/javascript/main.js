aa5aa850da6241712ce1d5b8e9683496d3957ed7
Tomas Aldirico <tomyaldirico@gmail.com>
2021-12-17 23:06:00 -0300

    Cambio de carpetas

---
+++ ejerciciosNico/javascript/main.js
@@ -0,0 +1,22 @@
+var popup = $('#popup');
+
+$(document).ready(function(){
+    showPopup();
+    hidePopup();
+    /* $("body").click(function(){
+        if (popup.css("display") == "none")
+            showPopup();
+        else
+            hidePopup();
+    }); */
+})
+
+
+function showPopup(){
+    popup.css("display","block");
+}
+function hidePopup(){
+    $('.cerrar').click(function(){
+        $('#popup').css("display", "none");
+    });
+}
